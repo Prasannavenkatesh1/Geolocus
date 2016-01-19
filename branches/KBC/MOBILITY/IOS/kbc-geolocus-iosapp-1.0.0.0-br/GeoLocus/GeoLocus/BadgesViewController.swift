9b55f86bac0a47359a7f8fe23866ec6e766fa139
372486 <372486@de9f4c7e-3401-0010-9731-c9be0b0bd99b>
2016-01-19 05:51:29 +0000

    added view controllers for menu items

---
+++ branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/BadgesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BadgesViewController.swift
+//  GeoLocus
+//
+//  Created by khan on 19/01/16.
+//  Copyright © 2016 Cognizant. All rights reserved.
+//
+
+import UIKit
+
+class BadgesViewController: UIViewController {
+
+}
---
+++ branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/ReportsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ReportsViewController.swift
+//  GeoLocus
+//
+//  Created by khan on 19/01/16.
+//  Copyright © 2016 Cognizant. All rights reserved.
+//
+
+import UIKit
+
+class ReportsViewController: UIViewController {
+
+}
