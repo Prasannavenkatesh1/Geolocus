6e9d033f577857a1cc7c2f7ec9c77529f85fd27f
372486 <372486@de9f4c7e-3401-0010-9731-c9be0b0bd99b>
2016-01-21 05:41:05 +0000

    added arcview controller

---
+++ GeoLocus/GeoLocus/ArcGraphicsController.swift
@@ -0,0 +1,18 @@
+//
+//  ArcGraphicsController.swift
+//  GeoLocus
+//
+//  Created by khan on 20/01/16.
+//  Copyright © 2016 Cognizant. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+ class ArcViewController: UIView {
+    
+    override func drawRect(rect: CGRect) {
+        
+    }
+    
+}
