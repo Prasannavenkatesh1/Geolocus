3654091c4277187fa9cd369a0def7b6286fc3e76
532973 <532973@de9f4c7e-3401-0010-9731-c9be0b0bd99b>
2016-01-22 04:48:28 +0000

    Login Page Integration

---
+++ branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  GeoLocus
+//
+//  Created by Saranya on 20/01/16.
+//  Copyright © 2016 Cognizant. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+}
