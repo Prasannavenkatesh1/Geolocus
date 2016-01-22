7e86a319314738c2bbe95dbcd27dffe78ec1d0af
458405 <458405@de9f4c7e-3401-0010-9731-c9be0b0bd99b>
2016-01-22 13:12:38 +0000

    Notification screen added

---
+++ branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/NotificationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationTableViewCell.swift
+//  GeoLocus
+//
+//  Created by sathishkumar on 20/01/16.
+//  Copyright © 2016 Cognizant. All rights reserved.
+//
+
+import UIKit
+
+class NotificationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
