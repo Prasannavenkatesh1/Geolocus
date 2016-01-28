e91aada189585f4114235f172877e4c4d7af33dd
528442 <528442@de9f4c7e-3401-0010-9731-c9be0b0bd99b>
2016-01-28 06:17:03 +0000

    Location Database update

--- branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/TimeSeriesModel.swift
+++ branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/TimeSeriesModel.swift
@@ -9,21 +9,23 @@
 import Foundation
 
 struct TimeSeriesModel {
-  let currenttime: String
+  let currenttime: NSDate
   let latitude: NSNumber
   let longitude: NSNumber
   let speed: NSNumber
-  let timezone: String
+  let datausage: NSNumber
+  let distance: NSNumber
   let isEvent: NSNumber
   let eventtype: NSNumber
   let eventvalue: NSNumber
   
-  init(ctime:String, lat:NSNumber, longt:NSNumber, speedval:NSNumber, tzone:String, iseventval:NSNumber, evetype:NSNumber, eveval:NSNumber){
+  init(ctime:NSDate, lat:NSNumber, longt:NSNumber, speedval:NSNumber, datausage:NSNumber, iseventval:NSNumber, evetype:NSNumber, eveval:NSNumber, distance:NSNumber){
     self.currenttime = ctime
     self.latitude = lat
     self.longitude = longt
     self.speed = speedval
-    self.timezone = tzone
+    self.datausage = datausage
+    self.distance = distance
     self.isEvent = iseventval
     self.eventtype = evetype
     self.eventvalue = eveval
