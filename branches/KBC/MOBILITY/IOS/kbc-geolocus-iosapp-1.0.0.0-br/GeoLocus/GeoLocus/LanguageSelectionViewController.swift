6375535db4728ecfacfef19453962267d5317448
532973 <532973@de9f4c7e-3401-0010-9731-c9be0b0bd99b>
2016-02-02 12:04:43 +0000

    Language Selection and Login View design updated

--- branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/LanguageSelectionViewController.swift
+++ branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/LanguageSelectionViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class LanguageSelectionViewController: UIViewController{
-    @IBAction func languageSelectedButton(sender: AnyObject){
-        print("Language Selected")
-        performSegueWithIdentifier("login", sender: self)
+    
+    @IBOutlet weak var layoutConstraintTop: NSLayoutConstraint!
+    
+    @IBAction func languageSelectedButton(sender: UIButton){
+        
+        var selectedLanguage : String!
+        
+        sender.backgroundColor = UIColor(red: 0, green: 174, blue: 239, alpha: 1.0)
+        sender.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        selectedLanguage = sender.titleLabel?.text
+        
+        switch(sender.tag){
+        case 1:
+            break
+        case 2,3,4:
+            performSegueWithIdentifier(StringConstants.LOGINVIEW_STORYBOARD_SEGUE, sender: self)
+            break
+        default:
+            break
+        }
+        
+        NSUserDefaults.standardUserDefaults().setValue(selectedLanguage, forKey: StringConstants.SELECTED_LANGUAGE_USERDEFAULT_KEY)
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    override func viewDidLoad() {
+        if(UIScreen.mainScreen().bounds.size.height < 568){
+            layoutConstraintTop.constant = 121
+        }
     }
 }
+
 
 
