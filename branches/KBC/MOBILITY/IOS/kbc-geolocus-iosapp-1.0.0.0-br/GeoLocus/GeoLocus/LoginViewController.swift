41e7a2acf3b4d6e65ad979acd9cb83d1be831341
532973 <532973@de9f4c7e-3401-0010-9731-c9be0b0bd99b>
2016-02-04 04:50:34 +0000

    Constraints modified for different device support

--- branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/LoginViewController.swift
+++ branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/LoginViewController.swift
@@ -7,19 +7,29 @@
 //
 
 import UIKit
-import TextFieldEffects
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController,UITextFieldDelegate {
     
     var isChecked = false
     
+    @IBOutlet weak var layoutConstraintTop: NSLayoutConstraint!
+    @IBOutlet weak var layoutConstraintVerticalUserNameTop: NSLayoutConstraint!
+    @IBOutlet weak var layoutConstraintLoginTop: NSLayoutConstraint!
+    @IBOutlet weak var layoutConstraintCheckBoxTop: NSLayoutConstraint!
+    @IBOutlet weak var layoutConstraintNeedHelpTop: NSLayoutConstraint!
+    @IBOutlet weak var layoutConstraintViewBottom: NSLayoutConstraint!
+    
+    @IBOutlet weak var userNameText: UITextField!
     @IBOutlet weak var needHelpButton: UIButton!
-    @IBOutlet weak var userNameText: HoshiTextField!
     @IBOutlet weak var registerNowButton: UIButton!
-    @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var checkButton: UIButton!
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var passwordText: UITextField!
     
+    let passwordShowButton = UIButton()
+    
+    // MARK: - Button Actions
+
     /* Register Now button action */
     @IBAction func registerNowButtonTapped(sender: AnyObject) {
         UIApplication.sharedApplication().openURL(NSURL(string:StringConstants.REGISTER_NOW_URL)!)
@@ -50,19 +60,44 @@ class LoginViewController: UIViewController {
         validate()
     }
     
+    // MARK: - View Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        userNameText.delegate = self
+        passwordText.delegate = self
+        
+        registerForKeyboardNotifications()
+        setConstraintsForDevice()
         customizeTextField()
         customizeButton()
     }
     
+    override func viewWillDisappear(animated: Bool) {
+        deregisterFromKeyboardNotifications()
+    }
+    
+    /* setting constraints based on device height */
+    func setConstraintsForDevice(){
+        if(StringConstants.SCREEN_HEIGHT < 568){
+            layoutConstraintTop.constant = 40
+            layoutConstraintVerticalUserNameTop.constant = 10
+            layoutConstraintLoginTop.constant = 10
+            layoutConstraintCheckBoxTop.constant = 10
+            layoutConstraintNeedHelpTop.constant = 5
+        }
+        if(StringConstants.SCREEN_HEIGHT == 568){
+            layoutConstraintTop.constant = 80
+            layoutConstraintLoginTop.constant = 30
+        }
+    }
+    
+    // MARK: - Custom Methods
+
     /* function to validate for required fields */
     func validate(){
         if !(userNameText.text!.isEmpty) && !(passwordText.text!.isEmpty) && (isChecked == true) {
-            // if(validateEmailAddress(userNameText.text!)){
             loginButton.enabled = true
-            // }
         }
     }
     
@@ -105,14 +140,32 @@ class LoginViewController: UIViewController {
         passwordBorder.borderWidth = CGFloat(1.0)
         passwordText.layer.addSublayer(passwordBorder)
         passwordText.layer.masksToBounds = true
+        
+        /* adding eye image to textfield */
+        passwordShowButton.setImage(UIImage(named: StringConstants.PASSWORD_EYE_IMAGE), forState: UIControlState.Normal)
+        passwordShowButton.frame = CGRectMake(passwordBorder.frame.size.width - 50, passwordText.bounds.origin.y, 20, 20)
+        passwordShowButton.addTarget(self, action:"eyeButtonTapped", forControlEvents: UIControlEvents.TouchUpInside)
+        passwordShowButton.contentMode = UIViewContentMode.Center
+        passwordText.rightViewMode = UITextFieldViewMode.WhileEditing
+        passwordText.rightView = passwordShowButton
     }
     
+    /* adding border to need help button */
     func customizeButton(){
-        /* adding border to need help button */
         let attributes = [NSUnderlineStyleAttributeName: NSUnderlineStyle.StyleSingle.rawValue]
         let attributedText = NSAttributedString(string: needHelpButton.currentTitle!, attributes: attributes)
         
         needHelpButton.titleLabel?.attributedText = attributedText
+    }
+    
+    /* method action when password show/hide button pressed */
+    func eyeButtonTapped(){
+        if(passwordText.secureTextEntry == true){
+            passwordText.secureTextEntry = false
+        }
+        else{
+            passwordText.secureTextEntry = true
+        }
     }
     
     /* clear the values of the textfield when navigated back from Register or Need Help page */
@@ -123,14 +176,6 @@ class LoginViewController: UIViewController {
         loginButton.enabled = false
         loginButton.backgroundColor = UIColor(red: 247.0/255.0, green: 249.0/255.0, blue: 251.0/255.0, alpha: 1.0)
         loginButton.setTitleColor(UIColor.lightGrayColor(),forState: UIControlState.Normal)
-    }
-    
-    /* function to validate for valid email address */
-    func validateEmailAddress(emailString:String) -> Bool{
-        let emailRegEx = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
-        
-        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        return emailTest.evaluateWithObject(emailString)
     }
     
     func checkUserDetails() -> Bool{
@@ -147,4 +192,55 @@ class LoginViewController: UIViewController {
         }
         return firstTimeLogin
     }
+    
+    //MARK : Notification methods on Keyboard pop up
+    
+    func registerForKeyboardNotifications()
+    {
+        //Adding notifies on keyboard appearing
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillBeHidden:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    
+    func deregisterFromKeyboardNotifications()
+    {
+        //Removing notifies on keyboard appearing
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWasShown(notification: NSNotification) {
+        
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            
+            if(StringConstants.SCREEN_HEIGHT < 568){
+                self.layoutConstraintVerticalUserNameTop.constant = 5
+                self.layoutConstraintTop.constant = 10
+            }
+            if(StringConstants.SCREEN_HEIGHT == 568){
+                self.layoutConstraintTop.constant = 60
+            }
+        })
+    }
+
+    func keyboardWillBeHidden(notification:NSNotification){
+        
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            if(StringConstants.SCREEN_HEIGHT < 568){
+                self.layoutConstraintVerticalUserNameTop.constant = 10
+                self.layoutConstraintTop.constant = 40
+            }
+            if(StringConstants.SCREEN_HEIGHT == 568){
+                self.layoutConstraintTop.constant = 80
+            }
+            
+        })
+    }
+    // MARK: Textfield Delegates Implementation
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
