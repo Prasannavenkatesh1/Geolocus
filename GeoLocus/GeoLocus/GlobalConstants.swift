35d38deef4a486bc19b81a77a209976c2851b27d
496606 <496606@de9f4c7e-3401-0010-9731-c9be0b0bd99b>
2016-02-04 10:55:45 +0000

    1 badges images 2overall screen 3 history changes

--- GeoLocus/GeoLocus/GlobalConstants.swift
+++ GeoLocus/GeoLocus/GlobalConstants.swift
@@ -149,3 +149,13 @@ public extension NSDate {
     }
 }
 
+public extension UIColor {
+    func colorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor {
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+        
+        return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
+    }
+}
+
