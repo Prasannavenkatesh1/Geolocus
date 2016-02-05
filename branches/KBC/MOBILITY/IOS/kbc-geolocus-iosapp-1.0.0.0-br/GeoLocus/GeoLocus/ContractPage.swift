4e3430cc9c1ad29b7560b7884b298c5a54a732b2
532973 <532973@de9f4c7e-3401-0010-9731-c9be0b0bd99b>
2016-02-05 12:28:13 +0000

    Contract screen modified to support all devices

--- branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/ContractPage.swift
+++ branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/ContractPage.swift
@@ -12,7 +12,14 @@ class ContractPage: UIViewController,UIImagePickerControllerDelegate,UINavigatio
     
     var imageViewBorder:CAShapeLayer!
     var imagePicker = UIImagePickerController()
-
+    let bottomBorder = CALayer()
+    
+    @IBOutlet weak var layoutConstraintBonusPointsTop: NSLayoutConstraint!
+    @IBOutlet weak var layoutConstraintEcoPointsViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var layoutConstraintSpeedPointsViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var layoutConstraintBonusPointsViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var layoutConstraintPointsViewHeight: NSLayoutConstraint!
+    
     @IBOutlet weak var speedPointsView: UIView!
     @IBOutlet weak var totalPointsLabel: UILabel!
     @IBOutlet weak var progressView: UIProgressView!
@@ -24,8 +31,10 @@ class ContractPage: UIViewController,UIImagePickerControllerDelegate,UINavigatio
     //MARK: View Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        setConstraintsForDifferentDevices()
         customiseProgressView()
         addDashedBorderToImageView()
+        addViewBorder()
         
         let tapGestureRecognizer = UITapGestureRecognizer(target:self, action:Selector("imageTapped:"))
         imageView.userInteractionEnabled = true
@@ -35,13 +44,39 @@ class ContractPage: UIViewController,UIImagePickerControllerDelegate,UINavigatio
     override func viewWillLayoutSubviews() {
         imageViewBorder.path = UIBezierPath(roundedRect: imageView.bounds, cornerRadius:2).CGPath
         imageViewBorder.frame = imageView.bounds
+        bottomBorder.frame = CGRect(x: speedPointsView.bounds.origin.x, y: speedPointsView.bounds.height, width: speedPointsView.bounds.width , height: 1.0)
     }
     
     //MARK: Custom Methods
     
+    /*set constraints for different device screen size */
+    func setConstraintsForDifferentDevices(){
+        if(StringConstants.SCREEN_HEIGHT < 568){
+            self.layoutConstraintBonusPointsTop.constant = 10
+            self.layoutConstraintEcoPointsViewHeight.constant = 35
+            self.layoutConstraintSpeedPointsViewHeight.constant = 35
+            self.layoutConstraintBonusPointsViewHeight.constant = 40
+            self.layoutConstraintPointsViewHeight.constant = 70
+        }
+        if(StringConstants.SCREEN_HEIGHT == 568){
+            self.layoutConstraintEcoPointsViewHeight.constant = 55
+            self.layoutConstraintSpeedPointsViewHeight.constant = 55
+            self.layoutConstraintPointsViewHeight.constant = 110
+        }
+    }
+    
     /* customize progress view */
     func customiseProgressView(){
         progressView.transform = CGAffineTransformScale(progressView.transform, 1, 10)
+    }
+    
+    /* adding border between the points view */
+    func addViewBorder(){
+        bottomBorder.borderColor = UIColor(red: 163.0/255.0, green: 162.0/255.0, blue: 162.0/255.0, alpha: 1.0).CGColor
+        bottomBorder.borderWidth = CGFloat(1.0)
+        bottomBorder.opaque = true
+        speedPointsView.layer.addSublayer(bottomBorder)
+        speedPointsView.layer.masksToBounds = true
     }
     
     /* Adding Border to ImageView */
