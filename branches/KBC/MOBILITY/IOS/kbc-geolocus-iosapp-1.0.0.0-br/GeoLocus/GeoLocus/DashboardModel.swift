b9cd310f701f46354198507c9b3edaa127fcfc7f
372486 <372486@de9f4c7e-3401-0010-9731-c9be0b0bd99b>
2016-02-09 11:46:44 +0000

    Dashboard data model added

---
+++ branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/DashboardModel.swift
@@ -0,0 +1,42 @@
+//
+//  DashboardModel.swift
+//  GeoLocus
+//
+//  Created by khan on 09/02/16.
+//  Copyright © 2016 Cognizant. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+
+
+struct DashboardModel {
+    
+    let score: String
+    let levelName: String
+    let levelMessage: String
+    let distanceTravelled: String
+    let totalPoints: String
+    let pointsAchieved: String
+    let scoreMessage: String
+    let tripStatus: String
+    
+    init(score: String, levelName: String, levelMessage: String, distanceTravelled: String, totalPoints: String, pointsAchieved: String, scoreMessage: String,
+        tripStatus: String) {
+    
+    self.score = score
+    self.levelName = levelName
+    self.levelMessage = levelMessage
+    self.distanceTravelled = distanceTravelled
+    self.totalPoints = totalPoints
+    self.pointsAchieved = pointsAchieved
+    self.scoreMessage = scoreMessage
+    self.tripStatus  = tripStatus
+            
+    }
+    
+}
+
