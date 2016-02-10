c9518262d721665d090f514e28e29407ca098cbf
372486 <372486@de9f4c7e-3401-0010-9731-c9be0b0bd99b>
2016-02-10 13:32:21 +0000

    Reports UI Integrated

--- GeoLocus/GeoLocus/ReportsViewController.swift
+++ GeoLocus/GeoLocus/ReportsViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ReportsViewController: UIViewController {
     
+    @IBOutlet weak var groupBarView: UIView!
+    
+    override func viewDidLoad() {
+//        ReportsViewController
+        
+        let storyBoard = UIStoryboard(name: StringConstants.StoryBoardIdentifier, bundle: nil)
+        let groupBarVC = storyBoard.instantiateViewControllerWithIdentifier(StringConstants.GroupBarViewController) as? GroupBarViewController
+        self.addChildViewController(groupBarVC!)
+        groupBarVC!.groupBarViewFrame = groupBarView.frame
+        groupBarVC!.view.frame = groupBarView.frame
+        groupBarView.addSubview((groupBarVC?.view!)!)
+    }
+    
+    //MARK :- UIButton Actions
     
     @IBAction func backButtonTapped(sender: AnyObject) {
         let storyBoard = UIStoryboard(name: StringConstants.StoryBoardIdentifier, bundle: nil)
