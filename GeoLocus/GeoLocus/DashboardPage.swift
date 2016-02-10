5f3cc62405a7db4204aaf9bf4fbca1f020d9029e
372486 <372486@de9f4c7e-3401-0010-9731-c9be0b0bd99b>
2016-02-10 13:16:06 +0000

    Added assets for Dashboard page

--- GeoLocus/GeoLocus/DashboardPage.swift
+++ GeoLocus/GeoLocus/DashboardPage.swift
@@ -22,13 +22,16 @@ import UIKit
     @IBOutlet weak var startStopButton: UIButton!
     @IBOutlet weak var contractsPointsEarnedValue: UILabel!
     
+    @IBOutlet weak var distanceTravelledLabel: UILabel!
     
     
     //MARK: System Delegates
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        customiseProgressView()
+       //  customColorAndFontSetup()
     startStopButton.layer.cornerRadius = 15.0
         // Do any additional setup after loading the view.
         self.arcView.ringLayer.strokeColor = UIColor.redColor().CGColor
@@ -58,8 +61,43 @@ import UIKit
 //All internal methods are written inside dashboardpage extension
 extension DashboardPage {
     
-    
+    func customiseProgressView(){
+        pointsAchievedProgressView.transform = CGAffineTransformScale(pointsAchievedProgressView.transform, 1, 5)
+    }
 
+    func customColorAndFontSetup() {
     
+        levelName.textColor = UIColor(netHex: 0x181f29)
+        distanceTravelledLabel.textColor = UIColor(netHex: 0x181f29)
+        distanceTravelledValue.textColor = UIColor(netHex: 0x00acef)
+     //   pointsAchievedProgressView.backgroundColor = UIColor(netHex: 0xacbdcd)
+        pointsAchievedProgressView.trackTintColor = UIColor(netHex: 0x00acef)
+        
+        
+        
+        let attributedTextColor1 =
+            [NSForegroundColorAttributeName: UIColor(netHex: 0x181f29),
+            NSFontAttributeName: UIFont(name: "HelveticaNeue", size: 16)!]
+        let attributedTextColor2 = [NSForegroundColorAttributeName: UIColor(netHex: 0x00acef),
+                                   NSFontAttributeName: UIFont(name: "HelveticaNeue-Medium", size: 16)! ]
+        let range1 = NSRange(location: 1, length: 23)
+        let range2 = NSRange(location: 24, length: 4)
+        
+        
+        let attributedString = NSMutableAttributedString(string: contractsPointsEarnedValue.text!, attributes: attributedTextColor1)
+        attributedString.addAttributes(attributedTextColor1, range: range1)
+        attributedString.addAttributes(attributedTextColor2, range: range2)
+        contractsPointsEarnedValue.attributedText = attributedString
+        
+        //53b262
+       // aeaeae
+        let buttonTextAttribute = [ NSFontAttributeName: UIFont(name: "HelveticaNeue", size: 16.0)!,
+            NSBackgroundColorAttributeName: UIColor(netHex: 0x53b262)]
+        
+        let finalButtonAttributes = NSMutableAttributedString(string: (startStopButton.titleLabel?.text)!, attributes: buttonTextAttribute)
+        startStopButton.titleLabel?.attributedText = finalButtonAttributes
+        startStopButton.backgroundColor = UIColor(netHex: 0xaeaeae)
+        
+    }
     
 }
