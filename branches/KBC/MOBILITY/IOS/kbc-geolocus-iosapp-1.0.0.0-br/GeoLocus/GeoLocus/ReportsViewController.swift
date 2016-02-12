09ceb560069be4182298318ee5cd81bb2e382348
372486 <372486@de9f4c7e-3401-0010-9731-c9be0b0bd99b>
2016-02-12 10:17:44 +0000

    reports filter popup integrated

--- branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/ReportsViewController.swift
+++ branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/ReportsViewController.swift
@@ -12,7 +12,15 @@ class ReportsViewController: UIViewController {
     
     @IBOutlet weak var groupBarView: UIView!
     @IBOutlet weak var filterPopUpView: UIView!
-    @IBOutlet weak var closePopUpBtn: UIButton!
+    @IBOutlet weak var weeklyBtn: UIButton!
+    @IBOutlet weak var monthlyBtn: UIButton!
+    @IBOutlet weak var speedBtn: UIButton!
+    @IBOutlet weak var EcoScoreBtn: UIButton!
+    @IBOutlet weak var attentionBtn: UIButton!
+    @IBOutlet weak var overallScoreBtn: UIButton!
+    @IBOutlet weak var okBtn: UIButton!
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     
     var groupBarVC: GroupBarViewController?
     
@@ -20,7 +28,13 @@ class ReportsViewController: UIViewController {
     }
     
     override func viewDidLayoutSubviews() {
-        
+
+        if UIScreen.mainScreen().bounds.size.height == 480 {
+            heightConstraint.constant = 240
+            bottomConstraint.constant = 20
+            self.view.layoutIfNeeded()
+        }
+
         if groupBarVC == nil {
             let storyBoard = UIStoryboard(name: StringConstants.StoryBoardIdentifier, bundle: nil)
             groupBarVC = (storyBoard.instantiateViewControllerWithIdentifier(StringConstants.GroupBarViewController) as? GroupBarViewController)!
@@ -38,12 +52,61 @@ class ReportsViewController: UIViewController {
         let navigationView = UINavigationController(rootViewController: rootView)
         self.revealViewController().setFrontViewController(navigationView, animated: true)
     }
- 
+    
     @IBAction func didTapOnFilterBtn(sender: UIButton) {
+        okBtn.layer.cornerRadius = 15.0
         filterPopUpView.hidden = false
     }
-
+    
     @IBAction func didTapOnCloseBtn(sender: UIButton) {
         filterPopUpView.hidden = true
+    }
+    
+    @IBAction func didTapOnTimeFrameBtn(sender: UIButton) {
+        
+        let btn: UIButton = sender
+        btn.selected = btn.selected ? false : true
+        
+        switch sender.tag {
+        case 100:
+            monthlyBtn.selected = false
+            break
+        case 101:
+            weeklyBtn.selected = false
+            break
+        default:
+            break
+        }
+    }
+    
+    @IBAction func didTapOnScoreOptionsBtn(sender: UIButton) {
+        
+        let btn: UIButton = sender
+        btn.selected = btn.selected ? false : true
+        
+        switch sender.tag {
+        case 1000:
+            EcoScoreBtn.selected = false
+            attentionBtn.selected = false
+            overallScoreBtn.selected = false
+            break
+        case 1001:
+            speedBtn.selected = false
+            attentionBtn.selected = false
+            overallScoreBtn.selected = false
+            break
+        case 1002:
+            speedBtn.selected = false
+            EcoScoreBtn.selected = false
+            overallScoreBtn.selected = false
+            break
+        case 1003:
+            speedBtn.selected = false
+            EcoScoreBtn.selected = false
+            attentionBtn.selected = false
+            break
+        default:
+            break
+        }
     }
 }
