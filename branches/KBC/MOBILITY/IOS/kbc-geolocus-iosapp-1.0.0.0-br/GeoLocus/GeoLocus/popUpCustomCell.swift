c29fee9f26096e45636d2e76218ecfa9006e2cb5
372486 <372486@de9f4c7e-3401-0010-9731-c9be0b0bd99b>
2016-02-19 07:54:28 +0000

    added popup in settings contoller

---
+++ branches/KBC/MOBILITY/IOS/kbc-geolocus-iosapp-1.0.0.0-br/GeoLocus/GeoLocus/popUpCustomCell.swift
@@ -0,0 +1,16 @@
+//
+//  popUpCustomCell.swift
+//  GeoLocus
+//
+//  Created by Harsh on 2/19/16.
+//  Copyright © 2016 Cognizant. All rights reserved.
+//
+
+import Foundation
+
+
+class popUpCustomCell : UITableViewCell{
+    
+    @IBOutlet weak var popUpDataUploadOrLanguageTypeLabel: UILabel!
+    @IBOutlet weak var popUpDataUploadOrLanguageTypeButton: UIButton!
+}
